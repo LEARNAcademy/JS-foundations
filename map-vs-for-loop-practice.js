82a51b70ed1af8427561f320d503c18e2887b1cb
Jezreel <jezreel.go@gmail.com>
2018-02-26 14:07:21 -0800

    Add map v for loop practice

---
+++ map-vs-for-loop-practice.js
@@ -0,0 +1,53 @@
+
+// Map vs. For Loop Practice
+
+// Below are exercises to get you seeing the differences between using a map and a for loop. Beneath each prompt write the code fo fulfill the exercise requirement.
+
+
+// Exercise 1
+// Consider this variable:
+var nums = [1, 2, 3, 4, 5]
+// Write a for loop that saves the result of each number multiplied by 5 to another array.
+
+// Exercise 2
+// Use a map on the nums array that saves the result of each number multiplied by 5 to another array.
+
+// Exercise 3
+// Consider this variable:
+var numbers = [11, 22, 33, 44, 55, 66]
+// Write a for loop that saves all numbers in the numbers array that are evenly divisible by 3 into another array.
+
+// Exercise 4
+// Use map on the numbers array to save any numbers from the array that are evenly divisible by 3 into another array.
+
+// Exercise 5
+// Consider this variable:
+var toons = [{name: "Tom", animal: "cat"}, {name: "Jerry", animal: "mouse"}, {name: "Bugs", animal: "rabbit"}, {name: "Daffy", animal: "duck"}, {name: "Felix", animal: "cat"}, {name: "Oswald", animal: "rabbit"}]
+// Write a for loop that saves all cat characters to another array.
+
+// Exercise 6
+// Use map on the toons array to save all rabbit characters to another array.
+
+// Exercise 7
+// Write a for loop to save the names of each character to another array.
+
+// Exercise 8
+// Use map on the toons array to save a description of each character to another array.
+
+// Exercise 9
+// Consider this variable:
+var people = [{firstName: "Alan", lastName: "Grant"}, {firstName: "Ellie", lastName: "Sattler"}, {firstName: "Ian", lastName: "Malcolm"}]
+// Write a for loop that creates a new object for every person that has a key value for firstName, lastName, and fullName and saves each new object into another array.
+
+// Exercise 10
+// Refactor the above code to use a map on the people array to achieve the same result.
+
+// Exercise 11
+// Consider this function:
+function addTitle(person){
+  return "Dr. " + person.firstName + " " + person.lastName
+}
+// Write a for loop that iterates through each person in the people array and stores the result of addTitle on each element to another array.
+
+// Exercise 12
+// Use map on the run addTitle on each element in the people array and store the result of each call into another array.
