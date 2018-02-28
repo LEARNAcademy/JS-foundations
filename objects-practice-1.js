cc2381d5079fa98969eb3038bec55aead80df9a1
Jezreel <jezreel.go@gmail.com>
2018-02-28 11:54:17 -0800

    add first objects practice set

---
+++ objects-practice-1.js
@@ -0,0 +1,35 @@
+
+// Objects Practice 1: Accessing Properties
+
+// Below are exercises in accessing the properties of Javascript Objects. Beneath each prompt write the code to fulfill the exercise requirement.
+
+// Exercise 1
+// Consider this variable:
+var person = { fName: "Arthur", lastName: "Dent" }
+// Write the code that uses dot notation to access the first name of the person object.
+
+// Exercise 2
+// Write the code that uses dot notation to access the last name of the person object.
+
+// Exercise 3
+// Write the code that uses bracket notation to access the first name of the person object.
+
+// Exercise 4
+// Write the code that uses bracket notation to access the last name of the persion object.
+
+// Exercise 5
+// Update the person object to have a method called fullName that returns the first and last name of the person object in one string. Call the method below.
+
+// Exercise 6
+// Consider this variable.
+var lunch = { name: "PB and Banana", type: "sandwich", ingredients: ["bread", "peanut butter", "banana"] }
+// Write the code that accesses the ingredients property of the lunch object. Provide the answer in dot and bracket notation.
+
+// Exercise 7
+// Write the code that access the first item in the ingredients property of the lunch object. Provide the answer in dot and bracket notation.
+
+// Exercise 8
+// Write a function that takes an object like the one above and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+// Exercise 9
+// Add a method to the object that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
