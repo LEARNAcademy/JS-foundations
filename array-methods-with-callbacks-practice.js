00c74a9ef46a7c92da1f898f15f88165278324b7
Jezreel <jezreel.go@gmail.com>
2018-03-05 15:23:54 -0800

    add array methods practice sets

---
+++ array-methods-with-callbacks-practice.js
@@ -0,0 +1,38 @@
+
+// Array Methods with Callbacks Practice
+
+// Below are exercises in using array methods that use callbacks. Beneath each prompt write the code to fulfill the exercise requirement.
+
+// Exercise 1
+// Consider this variable:
+var numbers = [1, 5, 3, 7, 8]
+// Write a function called isOdd that determines if a number is odd. Then use the filter method on the numbers array using the isOdd function as the callback to return all the odd numbers from the numbers array.
+
+// Exercise 2
+// Write a function called isEven that determines if a number is even. Then use the filter method on the numbers array using the isEven function as the callback to return all the even numbers from the  numbers array.
+
+// Exercise 3
+// Consider this variable:
+var sites = ["codepen.io", "codecademy.com", "coursera.org", "egghead.io", "udemy.com", "pluralsight.com", "udacity.com", "sitepoint.com"]
+// Write a function called dotCom that determines if a string contains .com. Then use the filter method on the sites array using the dotCom function as the callback to return all the sites that contain .com.
+
+// Exercise 4
+// Write a function called dotIo that determines if a string contains .io. Then use the filter method on the sites array using the dotIo function as the callback to return all the sites that contain .io.
+
+// Exercise 5
+// Write a function called dotOrg that determines if a string contains .org. Then use the filter method on the sites array using the dotOrg function as the callback to retrun all teh sites that contain .org.
+
+// Exercise 6
+// Write the code that uses an array method that takes the dotCom function as a callback and returns whether or not the sites array contains any .com sites.
+
+// Exercise 7
+// Write the code that uses an array method that takes the dotCom function as a callback and returns whether or not the sites array only contains .com sites.
+
+// Exercise 8
+// Write the code that uses an array method that takes the dotIo function as a callback and returns whether or not the sites array contains any .io sites.
+
+// Exercise 9
+// Write the code that uses an array method that takes the dotIo function as a callback and returns whether or not the sites array only contains .io sites.
+
+// Exercise 10
+// Write the code that uses an array method that takes the dotCom function as a callback and returns the first instance of a .com site. 
