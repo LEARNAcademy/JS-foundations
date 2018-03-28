eabe25b44389439595913b6432c8f06da6ac2cec
alyssamhope <berkeleycole@users.noreply.github.com>
2018-03-28 10:58:17 -0700

    Update loops.js

--- loops.js
+++ loops.js
@@ -2,18 +2,29 @@
 
 // 1. The classic for loop:
 
+// This is the most common type of loop you will see used in javascript. It gives you the most control over how you are iterating over items by letting you define:
+
+// 1. Where the count (index) starts 
+// 2. When the function should end (a condition that at some point returns false)
+// 3. What to do after each loop
+
 // -------------------------- EXAMPLE 1 ----------------------------------------------------
+
+// Can you guess what this loop will do?
 
 // for(var i=0; i < 5; i++){
 // 	console.log("Hello".repeat(i))
 // }
 
 // -------------------------- EXAMPLE 2 ----------------------------------------------------
-// FOR loops can also leave off the last argument
 
-// for(var i=0; i < 5; i++){
+// FOR loops can also leave off the last argument. Notice that examples 1 and 2 are functionally the same
+
+// for(var i=0; i < 5;){
 // 	console.log("Hello".repeat(i))
+//  i++
 // }
+
 // ===========================================================================================
 
 // The for in loop
