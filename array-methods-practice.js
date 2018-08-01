0aceff65988677b57b4f1db1df484511bbcbc2b6
alyssamhope <berkeleycole@users.noreply.github.com>
2018-08-01 10:25:13 -0700

    Update array-methods-practice.js

--- array-methods-practice.js
+++ array-methods-practice.js
@@ -1,48 +1,38 @@
 
-// Array Methods Practice
-
-// Below are exercises in using array methods. Beneath each prompt write the code to fulfill the exercise requirement.
-
-// Exercise 1
-// Consider this variable:
-var groceryList1 = ["apples", "carrots", "oatmeal"]
-// Write the code that will add "granola" to the end of array 
-
-// Exercise 2
-// Consider this variable:
-var groceryList2 = ["chips", "dip", "cookies"]
-// Write the code that will add "soda" to the end of the original array.
-
-// Exercise 3
-// Consider this variable:
-var numbers1 = [1, 2, 3, 4, 5]
-// Write the code that will add the number 0 to the beginning of the array 
-
-// Exercise 4
-// Consider this variable:
-var numbers2 = [2, 4, 6, 8, 10]
-// Write the code that will add the number 0 to the beginning of the original array.
-
-// Exercise 5
-// Consider this variable:
-var numbers3 = [2, 13, 6, 8, 4, 2]
-// Write the code that finds the index of the first appearance of the number 2.
-
-// Exercise 6
-// Write the code that finds the index of the last appearance of the number 2.
-
-// Exercise 7
-// Consider this variable:
-var chars = ["y", "a", "r", "r", "a"]
-// Write the code that brings all the letters in the chars array together into a string.
-
-// Exercise 8
-// Write the code that reverses the order of the letters in the chars array and saves it into a variable called charsReversed.
-
-// Exercise 9
-// Write the code that brings all the letters in the charsReversed array together into a string with a hyphen between each letter.
-
-// Exercise 10
-// Write the code that brings all the letters in the charsReversed Array together into a string without separators.
-
-
+ // Array Methods with Callbacks Practice		
+		
+ // Below are exercises in using array methods that use callbacks. Beneath each prompt write the code to fulfill the exercise requirement.		
+		
+ // Exercise 1		
+ // Consider this variable:		
+ var numbers = [1, 5, 3, 7, 8]		
+ // Write a function called isOdd that determines if a number is odd. Then use the filter method on the numbers array using the isOdd function as the callback to return all the odd numbers from the numbers array.		
+		
+ // Exercise 2		
+ // Write a function called isEven that determines if a number is even. Then use the filter method on the numbers array using the isEven function as the callback to return all the even numbers from the  numbers array.		
+		
+ // Exercise 3		
+ // Consider this variable:		
+ var sites = ["codepen.io", "codecademy.com", "coursera.org", "egghead.io", "udemy.com", "pluralsight.com", "udacity.com", "sitepoint.com"]		
+ // Write a function called dotCom that determines if a string contains .com. Then use the filter method on the sites array using the dotCom function as the callback to return all the sites that contain .com.		
+		
+ // Exercise 4		
+ // Write a function called dotIo that determines if a string contains .io. Then use the filter method on the sites array using the dotIo function as the callback to return all the sites that contain .io.		
+		
+ // Exercise 5		
+ // Write a function called dotOrg that determines if a string contains .org. Then use the filter method on the sites array using the dotOrg function as the callback to retrun all teh sites that contain .org.		
+		
+ // Exercise 6		
+ // Write the code that uses an array method that takes the dotCom function as a callback and returns whether or not the sites array contains any .com sites.		
+		
+ // Exercise 7		
+ // Write the code that uses an array method that takes the dotCom function as a callback and returns whether or not the sites array only contains .com sites.		
+		
+ // Exercise 8		
+ // Write the code that uses an array method that takes the dotIo function as a callback and returns whether or not the sites array contains any .io sites.		
+		
+ // Exercise 9		
+ // Write the code that uses an array method that takes the dotIo function as a callback and returns whether or not the sites array only contains .io sites.		
+		
+ // Exercise 10		
+ // Write the code that uses an array method that takes the dotCom function as a callback and returns the first instance of a .com site. 
