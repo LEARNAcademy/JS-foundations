3c3d5527aafa913bdc8efbb0be60b503b525a047
SunnyD <36974912+damonlemmons@users.noreply.github.com>
2018-11-26 16:51:22 -0800

    Create decisions.js

---
+++ decisions.js
@@ -0,0 +1,11 @@
+//write an if / else statement that logs "eat food" if you are hungry and "keep working" if you are not hungry.
+
+//write an if / else statement that logs "drink coffee" if you are tired and "keep going" if you are not tired.   
+
+//write an if / else statement that logs "in budget" if a price is lower than $250.  
+
+//write an if / else statement that considers two variables (numbers) and outputs the larger number. 
+
+//Write an if / else statement that considers a variable (number) and logs whether the number is odd or even.
+
+//Write an if / else statement that consdiers a variable (fruit) and logs the color of that fruit.
